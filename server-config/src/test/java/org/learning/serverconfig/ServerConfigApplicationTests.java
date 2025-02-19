9739f73893378bf405161118969ed913bd2e8b71
Chippo156 <vovannghiahiep150623@gmail.com>
2025-02-19 13:22:01 +0700

    Hiep : implement config server

---
+++ server-config/src/test/java/org/learning/serverconfig/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.serverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
