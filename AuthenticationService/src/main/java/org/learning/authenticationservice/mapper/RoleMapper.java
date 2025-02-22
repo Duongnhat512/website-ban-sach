fe9d0150356831e99e3b3124db9e25fb21fac2f2
Gia Bao <baoliengianguyen@gmail.com>
2025-02-22 12:53:35 +0700

    user role mapper

--- AuthenticationService/src/main/java/org/learning/authenticationservice/mapper/RoleMapper.java
+++ AuthenticationService/src/main/java/org/learning/authenticationservice/mapper/RoleMapper.java
@@ -1,2 +1,18 @@
-package org.learning.authenticationservice.mapper;public class RoleMapper {
+package org.learning.authenticationservice.mapper;
+import org.learning.authenticationservice.dto.request.RoleRequest;
+import org.learning.authenticationservice.dto.response.RoleResponse;
+import org.learning.authenticationservice.model.Role;
+import  org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RoleMapper {
+    Role toRole(RoleRequest request);
+    RoleResponse toRoleResponse(Role role);
+    Role toRole(RoleResponse response);
+    List<RoleResponse> toRoleList(List<Role> roles);
+
+
+
 }
--- AuthenticationService/src/main/java/org/learning/authenticationservice/mapper/UserMapper.java
+++ AuthenticationService/src/main/java/org/learning/authenticationservice/mapper/UserMapper.java
@@ -1,2 +1,16 @@
-package org.learning.authenticationservice.mapper;public interface UserMapper {
+package org.learning.authenticationservice.mapper;
+
+import org.learning.authenticationservice.dto.request.UserRequest;
+import org.learning.authenticationservice.dto.response.UserResponse;
+import org.learning.authenticationservice.model.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toUser(UserRequest request);
+    UserResponse toUserResponse(User user);
+    User toUser(UserResponse response);
+    List<UserResponse> toUserList(List<User> users);
 }
