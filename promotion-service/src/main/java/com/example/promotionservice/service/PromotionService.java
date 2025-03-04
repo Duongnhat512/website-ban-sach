ebb388715d1f59421bedd7a92a0bdc28b802e411
Duong Nhat <duongnhat512@gmail.com>
2025-03-04 14:26:46 +0700

    Duong-promotion v2: get all promotion

--- promotion-service/src/main/java/com/example/promotionservice/service/PromotionService.java
+++ promotion-service/src/main/java/com/example/promotionservice/service/PromotionService.java
@@ -3,8 +3,11 @@ package com.example.promotionservice.service;
 import com.example.promotionservice.dto.request.PromotionCreateRequest;
 import com.example.promotionservice.dto.response.PromotionCreateResponse;
 
+import java.util.List;
+
 public interface PromotionService {
     PromotionCreateResponse createPromotion(PromotionCreateRequest request);
     PromotionCreateResponse getPromotion(Long promotionId);
     PromotionCreateResponse updateAmountPromotion(Long promotionId, Long amount);
+    List<PromotionCreateResponse> getAllPromotions();
 }
