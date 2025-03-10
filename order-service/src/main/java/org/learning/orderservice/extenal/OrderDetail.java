fff81c0c6fcbd996d309cd746d05b5b8f0898dd3
Chippo156 <vovannghiahiep150623@gmail.com>
2025-03-10 16:44:28 +0700

    Hiep : update orderDetail call create order

---
+++ order-service/src/main/java/org/learning/orderservice/extenal/OrderDetail.java
@@ -0,0 +1,36 @@
+package org.learning.orderservice.extenal;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "order-details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class OrderDetail {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "total")
+    private Double total;
+}
