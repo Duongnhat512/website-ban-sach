893a7ec5d4467cb4057908e942f5dfbdc4337f18
Duong Nhat <duongnhat512@gmail.com>
2025-03-31 15:45:38 +0700

    Duong: update kieu du lieu text description

--- book-service/src/main/java/com/example/bookservice/entity/Book.java
+++ book-service/src/main/java/com/example/bookservice/entity/Book.java
@@ -19,6 +19,7 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String description;
     private String author;
     private Double originalPrice;
